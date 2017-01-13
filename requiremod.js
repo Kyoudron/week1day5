2073a9af881ea9a809fdf9adef2f54ffc4f533a1
Kyoudron <kapish.k.1@gmail.com>
2017-01-13 11:05:37 -0500

    Update requiremod.js

--- requiremod.js
+++ requiremod.js
@@ -2,6 +2,7 @@ module.exports = {
   fruit: "apple",
   vegetable: "lettuce",
   howMuch: function cost() {
+    console.log("My favorite thing to eat is " + this.fruit);
     var privateVar = function(){
       myPrivateVar = 25
      }
